f511ebd0c24eee119523dc97ad0847fda4a2ab2f
sanchezguz2003 <106396334+sanchezguz2003@users.noreply.github.com>
2022-08-17 14:09:02 -0500

    Add files via upload

---
+++ excepciones/Excepciones.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones;
+
+/**
+ *
+ * @author cheli
+ */
+public class Excepciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String mensaje=null;
+        //try{
+        mensaje.equals("hola");
+        //}catch(exception ex){
+            //System.out.println("Error"+ ex.getMessage());
+    }
+}
